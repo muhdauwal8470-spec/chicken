1c81fd13f1a4fa862e1cb02d065c29e74291203e
muhdauwal8470-spec <muhdauwal8470@gmail.com>
2026-06-22 07:04:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import streamlit as st
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
-from PIL import Image
-
-class_names = ['healthy', 'sick']
 
 model = load_model('chicken_disease_model.h5')
 
@@ -14,8 +11,7 @@ st.write('Upload a chicken image to check its health status.')
 uploaded_file = st.file_uploader('Upload Chicken Image', type=['jpg', 'jpeg', 'png'])
 
 if uploaded_file:
-    image = Image.open(uploaded_file).convert('RGB')
-    st.image(image, caption='Uploaded image')
+    st.image(uploaded_file, caption='Uploaded image')
 
     if st.button('Predict'):
         img = image.resize((224, 224))
@@ -23,6 +19,5 @@ if uploaded_file:
         img_array = np.expand_dims(img_array, axis=0)
 
         prediction = model.predict(img_array)
-        predicted_class = class_names[np.argmax(prediction)]
 
-        st.subheader(f'Prediction: {predicted_class}')
+        st.subheader(f'Prediction: {prediction}')
